01de89578b463100577a11721bf869b1823bf55e
Brendan Graetz <bguiz@users.noreply.github.com>
2023-05-26 11:37:10 +0800

    proj: remove 11ty upgrade helper as its job is done

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 
 const rssPlugin = require('@11ty/eleventy-plugin-rss');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const eleventyUpgradeHelper = require("@11ty/eleventy-upgrade-help");
 
 // Import filters
 const dateFilter = require('./src/filters/date-filter.js');
@@ -121,7 +120,6 @@ module.exports = function(config) {
   // Plugins
   config.addPlugin(rssPlugin);
   config.addPlugin(syntaxHighlight);
-  config.addPlugin(eleventyUpgradeHelper);
 
   // Ref: https://www.11ty.dev/docs/data-deep-merge/
   // Ref: https://github.com/11ty/eleventy-upgrade-help/tree/v1.x#data-deep-merge
